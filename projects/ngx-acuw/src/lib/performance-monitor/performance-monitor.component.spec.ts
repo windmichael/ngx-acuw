05ac4451d6d6eb09380dced0409dc1441c444f6c
Michael Wind <michael.wind@outlook.at>
2021-08-13 13:07:17 +0200

    add min. and max. fps to the performance monitor

--- projects/ngx-acuw/src/lib/performance-monitor/performance-monitor.component.spec.ts
+++ projects/ngx-acuw/src/lib/performance-monitor/performance-monitor.component.spec.ts
@@ -24,10 +24,11 @@ describe('PerformanceMonitorComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should show 0 fps after created', () => {
+  it('should not show any fps after created', () => {
     fixture.detectChanges();
-    const elem = fixture.debugElement.query(By.css('#fps-display')).nativeElement as HTMLDivElement;
-    expect(elem.textContent).toEqual(0 + ' FPS');
+    const elem = fixture.debugElement.query(By.css('#fps-display')).query(By.css('span'));
+    expect(elem).toBeNull();
+    expect(component.fps).toBe(-1);
   });
 
   it('should calculate the correct fps', fakeAsync(() => {
@@ -38,7 +39,33 @@ describe('PerformanceMonitorComponent', () => {
     }
 
     expect(component.fps).toEqual(10);
-    const elem = fixture.debugElement.query(By.css('#fps-display')).nativeElement as HTMLDivElement;
+    const elem = fixture.debugElement.query(By.css('#fps-display')).query(By.css('span')).nativeElement as HTMLSpanElement;
     expect(elem.textContent).toEqual(10 + ' FPS');
+  }));
+
+  it('should calculate the correct and min. and max fps', fakeAsync(() => {
+    // Simulate animation with 10 fps
+    for(let idx = 0; idx <= 11; idx ++){
+      tick(100);
+      component.end();
+    }
+    // Simulate animation with 20 fps
+    for(let idx = 0; idx <= 50; idx ++){
+      tick(50);
+      component.end();
+    }
+    // Simulate animation with 5 fps
+    for(let idx = 0; idx <= 11; idx ++){
+      tick(200);
+      component.end();
+    }
+
+    expect(component.fpsMin).toEqual(5);
+    let elem = fixture.debugElement.query(By.css('#min-fps-display')).nativeElement as HTMLSpanElement;
+    expect(elem.textContent).toEqual(5 + ' min');
+
+    expect(component.fpsMax).toEqual(20);
+    elem = fixture.debugElement.query(By.css('#max-fps-display')).nativeElement as HTMLSpanElement;
+    expect(elem.textContent).toEqual(20 + 'max');
   }));
 });
--- projects/ngx-acuw/src/lib/performance-monitor/performance-monitor.component.ts
+++ projects/ngx-acuw/src/lib/performance-monitor/performance-monitor.component.ts
@@ -3,25 +3,16 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/
 @Component({
   selector: 'acuw-performance-monitor',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: `
-    <div id="fps-display">{{ fps | number: '1.0-0' }} FPS</div>
-  `,
-  styles: [
-    `div {
-      position: absolute;
-      color: rgb(255, 255, 255);;
-      background-color: rgba(0, 0, 0, 0.8);
-      top: 5px;
-      left: 5px;
-      padding: 5px;
-    }`
-  ]
+  templateUrl: './performance-monitor.component.html',
+  styleUrls: ['./performance-monitor.component.css']
 })
 export class PerformanceMonitorComponent {
 
-  fps = 0;
+  fps = -1;
+  fpsMin = -1;
+  fpsMax = -1;
   private framesCnt = 0;
-  private prevFrameTime = 0;
+  private timeLastFpsCalc = 0;
 
   constructor(private changeDetector: ChangeDetectorRef) { }
 
@@ -29,18 +20,31 @@ export class PerformanceMonitorComponent {
    * Method to be called at the end of a frame
    */
   end(): void {
+    // When called the first time, set the current time and return
+    if (this.timeLastFpsCalc === 0) {
+      this.timeLastFpsCalc = Date.now();
+      return;
+    }
     // Increase the frames counter
     this.framesCnt++;
     // Get the milliseconds elapsed since January 1, 1970 for the current frame 
     const currentFrameTime = Date.now();
     // Calculate the FPS only every second
-    if (currentFrameTime >= this.prevFrameTime + 1000) {
-      // Caclulate the frames per second
-      this.fps = this.framesCnt / (currentFrameTime - this.prevFrameTime) * 1000;
+    if (currentFrameTime >= this.timeLastFpsCalc + 1000) {
+      // Calculate the frames per second
+      this.fps = Math.round(this.framesCnt / (currentFrameTime - this.timeLastFpsCalc) * 1000);
+      // Calculate the min. frames per second
+      if(this.fpsMin === -1 || this.fps < this.fpsMin){
+        this.fpsMin = this.fps;
+      }
+      // Calculate the max. frames per second
+      if(this.fpsMax === -1 || this.fps > this.fpsMax){
+        this.fpsMax = this.fps;
+      }
       // Trigger the change detection
       this.changeDetector.detectChanges();
-      // Set the elapsed time to the prevFrameTime -> for the next frame
-      this.prevFrameTime = currentFrameTime;
+      // Set the elapsed time to the timeLastFpsCalc
+      this.timeLastFpsCalc = currentFrameTime;
       // Rese the frames counter
       this.framesCnt = 0;
     }
